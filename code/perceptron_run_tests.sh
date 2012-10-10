4a2ecdba1f0587d19d379861718bcac0c90c5b91
Dan Crankshaw <dscrankshaw@gmail.com>
2012-10-09 23:56:01 -0400

    Added shell scripts to run new algorithms.

---
+++ code/perceptron_run_tests.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# $1 is the classifier
+
+#datasets[0]='bio'
+#datasets[1]='easy'
+#datasets[2]='finance'
+#datasets[3]='hard'
+#datasets[4]='nlp'
+#datasets[5]='speech'
+#datasets[6]='vision'
+datasets=( bio finance nlp speech vision easy hard )
+# datasets=(speech speech.mc )
+
+# CLASSIFIER=$1
+CLASSIFIER=perceptron
+TRAIN=./perceptron_train.sh
+TEST=./perceptron_test.sh
+
+for i in "${datasets[@]}"
+do
+#  echo "Train `${datasets[$i]}` : `$TRAIN $CLASSIFIER ${datasets[$i]}`"
+#  echo "Test `${datasets[$i]}` : `$TEST $CLASSIFIER ${datasets[$i]}`"
+  echo "Train $i : `$TRAIN $CLASSIFIER $i`"
+#  echo "Test $i : `$TEST $CLASSIFIER $i`"
+done
+
+echo
+
+for i in "${datasets[@]}"
+do
+#  echo "Train `${datasets[$i]}` : `$TRAIN $CLASSIFIER ${datasets[$i]}`"
+#  echo "Test `${datasets[$i]}` : `$TEST $CLASSIFIER ${datasets[$i]}`"
+#  echo "Train $i : `$TRAIN $CLASSIFIER $i`"
+  echo "Test $i : `$TEST $CLASSIFIER $i`"
+done
