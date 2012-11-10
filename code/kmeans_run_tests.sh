87d3e2692679adf02d2ca66d881bd351be42ec92
Dan Crankshaw <dscrankshaw@gmail.com>
2012-11-10 15:43:15 -0500

    Updated running scripts.

---
+++ code/kmeans_run_tests.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# $1 is the classifier
+
+#datasets=( bio finance nlp speech vision hard easy)
+datasets=(bio easy finance hard speech speech.mc vision nlp)
+
+# CLASSIFIER=$1
+CLASSIFIER=lambda_means
+TRAIN=./kmeans_train.sh
+TEST=./kmeans_test.sh
+
+for i in "${datasets[@]}"
+do
+  echo "Train $i : `$TRAIN $CLASSIFIER $i`"
+done
+
+echo
+
+for i in "${datasets[@]}"
+do
+  echo "Test $i : `$TEST $CLASSIFIER $i`"
+done
---
+++ code/kmeans_train.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# $1 is classifier
+# $2 is dataset (speech, bio, etc.)
+
+JAVA=`which java`
+
+# EXTRA_FILES=~/cs475-machine_learning/hw1/01_all_extra_files_1/
+CLASSPATH=.:~/cs475-machine_learning/commons-cli-1.2/commons-cli-1.2.jar:~/cs475-machine_learning/code/bin
+
+$JAVA -cp $CLASSPATH cs475.Classify -mode train -algorithm $1 \
+  -model_file model.$1.$2 -data ~/cs475-machine_learning/data/$2.train \
+  -clustering_training_iterations 10
