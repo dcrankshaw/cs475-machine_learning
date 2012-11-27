7372e8a4cca69562507e09ec1ae8b7e16f2eeeb2
Dan Crankshaw <dscrankshaw@gmail.com>
2012-11-26 22:30:50 -0500

    Added a bit to denoisify.

--- code/cs475/image_denoise/MRFImageProcessor.java
+++ code/cs475/image_denoise/MRFImageProcessor.java
@@ -33,6 +33,7 @@ public class MRFImageProcessor {
     }
 
     public int[][] denoisifyImage(int[][] image, int[][] IGNORE) {
+
         if (use_second_level_) {
             return denoisifyImageTwoLevels(image);
         } else {
@@ -42,11 +43,49 @@ public class MRFImageProcessor {
 
 
     private int[][] denoisifyImageOneLevel(int[][] image) {
-    
+        boolean greyscale = (ImageUtils.countColors(image) > 2);
+        // initialize hidden nodes
+        int[][] hidden_nodes = new int[image.length][];
+        for (int i = 0; i < image.length; ++i) {
+            hidden_nodes[i] = new int[image[i].length];
+            for (int j = 0; j < image[i].length; ++j) {
+                hidden_nodes[i][j] = image[i][j];
+            }
+        }
+        for (int iter = 0; iter < num_iterations_; ++iter) {
+            for (int ii = 0; ii < hidden_nodes.length; ++ii) {
+                for (int jj = 0; jj < hidden_nodes[ii].length; ++jj) {
+                    
+                }
+            }
+        }
+
+    }
+
+    private potentialHiddenHidden(int xi, int xj, boolean greyscale) {
+        if (greyscale) {
+            double potential = (Math.log(Math.abs(xj - xi) + 1) - 1) * beta_;
+        } else {
+            if (xi == xj)
+                return -1 * beta_;
+            else
+                return beta_;
+        }
+    }
+
+    private potentialHiddenObserved(int xi, int yi, boolean greyscale) {
+        if (greyscale) {
+            double potential = (Math.log(Math.abs(xi - yi) + 1) - 1) * beta_;
+        } else {
+            if (xi == yi)
+                return -1 * beta_;
+            else
+                return beta_;
+        }
     }
 
     private int[][] denoisifyImageTwoLevels(int[][] image) {
-    
+
     }
 
 
