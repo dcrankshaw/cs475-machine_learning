65feb66cd0d0e5aed114c1e14340c96e3acad439
Dan Crankshaw <dscrankshaw@gmail.com>
2012-11-28 20:08:15 -0500

    Fixed compile errors.

--- code/cs475/image_denoise/MRFImageProcessor.java
+++ code/cs475/image_denoise/MRFImageProcessor.java
@@ -1,13 +1,5 @@
 package cs475.image_denoise;
 
-import java.awt.image.BufferedImage;
-import java.util.HashMap;
-import java.util.LinkedList;
-
-import org.apache.commons.cli.Option;
-import org.apache.commons.cli.OptionBuilder;
-
-import cs475.CommandLineUtilities;
 
 public class MRFImageProcessor {
     double eta_;
@@ -32,36 +24,55 @@ public class MRFImageProcessor {
         second_level_ = use_second_level;
     }
 
-    private int[][] updateK(int[][] hidden_nodes, boolean greyscale) {
+    private int[][] updateK(int[][] hidden_nodes, boolean greyscale, int color1, int color2) {
         int maxKI = (hidden_nodes.length - 1) / num_K_;
         int maxKJ = (hidden_nodes[0].length - 1) / num_K_;
         int[][] k_nodes = new int[maxKI + 1][maxKJ + 1];
         int[][] k_nodes_members = new int[maxKI + 1][maxKJ + 1];
-        for (int ii = 0; ii < hidden_nodes.length; ++ii) {
-            for (int jj = 0; jj < hidden_nodes[0].length; ++jj) {
-                int ki = ii / num_K_;
-                int kj = jj / num_K_;
-                k_nodes[ki][kj] += hidden_nodes[ii][jj];
-                k_nodes_members[ki][kj] += 1;
+        if (greyscale) {
+            for (int ii = 0; ii < hidden_nodes.length; ++ii) {
+                for (int jj = 0; jj < hidden_nodes[0].length; ++jj) {
+                    int ki = ii / num_K_;
+                    int kj = jj / num_K_;
+                    k_nodes[ki][kj] += hidden_nodes[ii][jj];
+                    k_nodes_members[ki][kj] += 1;
+                }
             }
-        }
-        for (int ki = 0; ki k_nodes.length; ++ki) {
-            for (int kj = 0; kj < k_nodes[0].length; ++kj) {
-                if (greyscale) {
-                    float kavg = (float) k_nodes[ki][kj] / (float) k_nodes_members[ki][kj];
-                    int kavgInt = Math.round(kavg);
-                    k_nodes[ki][kj] = kavgInt;
-                } else {
-                    int pixelVal = Math.abs(hidden_nodes[0]);
-                    if (k_nodes[ki][kj] > 0) {
-                        k_nodes[ki][kj] = pixelVal;
-                    } else {
-                        k_nodes[ki][kj] = -1 * pixelVal;
-                    }
-
+            for (int ki = 0; ki < k_nodes.length; ++ki) {
+                for (int kj = 0; kj < k_nodes[0].length; ++kj) {
+                        double kavg = (double) k_nodes[ki][kj] / (double) k_nodes_members[ki][kj];
+                        int kavgInt = (int) Math.round(kavg);
+                        k_nodes[ki][kj] = kavgInt;
                 }
             }
         }
+        else {
+            for (int ii = 0; ii < hidden_nodes.length; ++ii) {
+                for (int jj = 0; jj < hidden_nodes[0].length; ++jj) {
+                    int ki = ii / num_K_;
+                    int kj = jj / num_K_;
+                    if (hidden_nodes[ii][jj] == color1) {
+                        k_nodes[ki][kj] += 1;
+                    } else {
+                        if (hidden_nodes[ii][jj] != color2) {
+                            System.out.println("bad color matching. grep this ksdfkjdhsjfk");
+                        }
+                        k_nodes[ki][kj] -= 1;
+                    }
+                }
+            }
+            for (int ki = 0; ki < k_nodes.length; ++ki) {
+                for (int kj = 0; kj < k_nodes[0].length; ++kj) {
+                    if (k_nodes[ki][kj] >= 0) {
+                        k_nodes[ki][kj] = color1;
+                    } else {
+                        k_nodes[ki][kj] = color2;
+                    }
+                }
+            }
+
+        }
+        return k_nodes;
     }
 
     private static int[][] copyArray(int[][] image) {
@@ -72,12 +83,13 @@ public class MRFImageProcessor {
                 hidden_nodes[i][j] = image[i][j];
             }
         }
+        return hidden_nodes;
     }
 
 
-    private int[][] denoisifyImage(int[][] image, int[][] IGNORE) {
-        boolean greyscale = (ImageUtils.countColors(image) > 2);
-        int maxColorValue = ImageUtils.maxColorValue(image);
+    public int[][] denoisifyImage(int[][] image, int[][] IGNORE) {
+        boolean greyscale = (ImageUtils.countColors(image, false) > 2);
+        int maxColorValue = ImageUtils.maxColor(image);
         int color1 = 0;
         int color2 = 0;
         if (!greyscale) {
@@ -94,17 +106,22 @@ public class MRFImageProcessor {
                 if (done) break;
             }
         }
+        if (color1 > color2) {
+            int temp = color1;
+            color1 = color2;
+            color2 = temp;
+        }
 
         // initialize hidden nodes
         int[][] hidden_nodes = copyArray(image);
-        if (use_second_level_) {
-            k_nodes = updateK(hidden_nodes, greyscale);
-        }
         int[][] k_nodes = null;
+        if (second_level_) {
+            k_nodes = updateK(hidden_nodes, greyscale, color1, color2);
+        }
         if (greyscale) {
             // For grey images
             for (int iter = 0; iter < num_iterations_; ++iter) {
-                //float InitialEnergy = calculateTotalEnergy(hidden_nodes, image, greyscale);
+                //double InitialEnergy = calculateTotalEnergy(hidden_nodes, image, greyscale);
                 int[][] newHiddenNodes = new int[hidden_nodes.length][hidden_nodes[0].length];
                 for (int ii = 0; ii < hidden_nodes.length; ++ii) {
                     for (int jj = 0; jj < hidden_nodes[ii].length; ++jj) {
@@ -112,15 +129,15 @@ public class MRFImageProcessor {
                     }
                 }
                 hidden_nodes = newHiddenNodes;
-                if (use_second_level_) {
-                    k_nodes = updateK(hidden_nodes, greyscale);
+                if (second_level_) {
+                    k_nodes = updateK(hidden_nodes, greyscale, color1, color2);
                 }
 
             }
         } else {
             // for black and white images
             for (int iter = 0; iter < num_iterations_; ++iter) {
-                //float InitialEnergy = calculateTotalEnergy(hidden_nodes, image, greyscale);
+                //double InitialEnergy = calculateTotalEnergy(hidden_nodes, image, greyscale);
                 int[][] newHiddenNodes = new int[hidden_nodes.length][hidden_nodes[0].length];
                 for (int ii = 0; ii < hidden_nodes.length; ++ii) {
                     for (int jj = 0; jj < hidden_nodes[ii].length; ++jj) {
@@ -128,23 +145,20 @@ public class MRFImageProcessor {
                     }
                 }
                 hidden_nodes = newHiddenNodes;
-                if (use_second_level_) {
-                    k_nodes = updateK(hidden_nodes, greyscale);
+                if (second_level_) {
+                    k_nodes = updateK(hidden_nodes, greyscale, color1, color2);
                 }
             }
         }
+        return hidden_nodes;
     }
 
     // This can be done locally without having to compute the total
     // energy. Just find the value that minizimizes the energy for the cliques that
     // this node is part of.
     private int findMinEnergyBW(int ii, int jj, int[][] hidden_nodes, int[][] image, int[][] k_nodes, int color1, int color2) {
-        int iMin = (ii - 1) < 0 ? 0 : ii - 1;
-        int jMin = (jj - 1) < 0 ? 0 : jj - 1;
-        int iMax = (ii + 1) < hidden_nodes.length ? (ii + 1) : hidden_nodes.length - 1;
-        int jMax = (jj + 1) < hidden_nodes[0].length ? (jj + 1) : hidden_nodes[0].length - 1;
-        float color1Energy = 0;
-        float color2Energy = 0;
+        double color1Energy = 0;
+        double color2Energy = 0;
         if ((ii - 1) >= 0) {
             color1Energy += potentialHiddenHidden(color1, hidden_nodes[ii-1][jj], false); 
             color2Energy += potentialHiddenHidden(color2, hidden_nodes[ii-1][jj], false); 
@@ -164,7 +178,7 @@ public class MRFImageProcessor {
         }
         color1Energy += potentialHiddenObserved(color1, image[ii][jj], false);
         color2Energy += potentialHiddenObserved(color2, image[ii][jj], false);
-        if (use_second_level_) {
+        if (second_level_) {
             int ki = ii / num_K_;
             int kj = jj / num_K_;
             color1Energy += potentialHiddenZ(color1, k_nodes[ki][kj], false);
@@ -178,14 +192,10 @@ public class MRFImageProcessor {
     }
 
     private int findMinEnergyGrey(int ii, int jj, int[][] hidden_nodes, int[][] image, int maxColorVal, int[][] k_nodes) {
-        int iMin = (ii - 1) < 0 ? 0 : ii - 1;
-        int jMin = (jj - 1) < 0 ? 0 : jj - 1;
-        int iMax = (ii + 1) < hidden_nodes.length ? (ii + 1) : hidden_nodes.length - 1;
-        int jMax = (jj + 1) < hidden_nodes[0].length ? (jj + 1) : hidden_nodes[0].length - 1;
-        float minEnergy = Float.MAX_VALUE;
+        double minEnergy = Float.MAX_VALUE;
         int minColor = 0;
         for (int color = 0; color <= maxColorVal; ++color) {
-            float curEnergy = 0;
+            double curEnergy = 0;
             if ((ii - 1) >= 0) {
                 curEnergy += potentialHiddenHidden(color, hidden_nodes[ii-1][jj], true); 
             }
@@ -200,7 +210,7 @@ public class MRFImageProcessor {
                 curEnergy += potentialHiddenHidden(color, hidden_nodes[ii][jj+1], true); 
             }
             curEnergy += potentialHiddenObserved(hidden_nodes[ii][jj], image[ii][jj], true);
-            if (use_second_level_) {
+            if (second_level_) {
                 int ki = ii / num_K_;
                 int kj = jj / num_K_;
                 curEnergy += potentialHiddenZ(hidden_nodes[ii][jj], k_nodes[ki][kj], true);
@@ -214,18 +224,19 @@ public class MRFImageProcessor {
     }
 
     // Don't need
-    private float calculateTotalEnergy(int[][] hidden_nodes, int[][] image, boolean greyscale) {
-        float energy = 0;
+    /*
+    private double calculateTotalEnergy(int[][] hidden_nodes, int[][] image, boolean greyscale) {
+        double energy = 0;
         // Calculate energy from horizontal cliques
         for (int row = 0; row < hidden_nodes.length; ++row) {
-            for (int i = 0; int j = 1; j < hidden_nodes[row].length; ++i, ++j) {
+            for (int i = 0, j = 1; j < hidden_nodes[row].length; ++i, ++j) {
                 energy+= potentialHiddenHidden(hidden_nodes[row][i], hidden_nodes[row][j], greyscale);
             }
         }
 
         // Calculate energy from vertical cliques - all columns have same length
         for (int column = 0; column < hidden_nodes[0].length; ++column) {
-            for (int i = 0; int j = 1; j < hidden_nodes.length; ++i, ++j) {
+            for (int i = 0, j = 1; j < hidden_nodes.length; ++i, ++j) {
                 energy+= potentialHiddenHidden(hidden_nodes[i][column], hidden_nodes[j][column], greyscale);
             }
         }
@@ -238,11 +249,13 @@ public class MRFImageProcessor {
         }
         return energy;
     }
+    */
 
 
-    private float potentialHiddenHidden(int xi, int xj, boolean greyscale) {
+    private double potentialHiddenHidden(int xi, int xj, boolean greyscale) {
         if (greyscale) {
             double potential = (Math.log(Math.abs(xj - xi) + 1) - 1) * beta_;
+            return potential;
         } else {
             if (xi == xj)
                 return -1 * beta_;
@@ -251,20 +264,22 @@ public class MRFImageProcessor {
         }
     }
 
-    private float potentialHiddenObserved(int xi, int yi, boolean greyscale) {
+    private double potentialHiddenObserved(int xi, int yi, boolean greyscale) {
         if (greyscale) {
-            double potential = (Math.log(Math.abs(xi - yi) + 1) - 1) * beta_;
+            double potential = (Math.log(Math.abs(xi - yi) + 1) - 1) * eta_;
+            return potential;
         } else {
             if (xi == yi)
-                return -1 * beta_;
+                return -1 * eta_;
             else
-                return beta_;
+                return eta_;
         }
     }
 
-    private float potentialHiddenZ(int xi, int zi, boolean greyscale) {
+    private double potentialHiddenZ(int xi, int zi, boolean greyscale) {
         if (greyscale) {
             double potential = (Math.log(Math.abs(xi - zi) + 1) - 1) * omega_;
+            return potential;
         } else {
             if (xi == zi)
                 return -1 * omega_;
